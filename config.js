20bf94605f6dffe88a35abc7a121ac88397d5fe7
ORYC <oryc9x@gmail.com>
2016-04-17 11:35:03 +0700

    update config file

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ var config = {
 		port: process.env.PORT
 	},
 	site: {
-		title: 'Siminimal',
-		name: 'Siminimal',
+		title: 'DungPS Blog',
+		name: 'DungPS',
 		url: 'http://dungps.herokuapp.com',
 		description: 'Simple Minimal Blogging Platform built with NodeJS. No database required. Markdown supports.',
 		author: 'DungPS',
-		keywords: "siminimal, blog, cms, simple, minimal"
+		keywords: "dungps, oryc, blog, wordpress, nodejs"
 	},
 	options: {
-		posts_per_page: 1,
+		posts_per_page: 5,
 		theme: 'mono'
 	}
 }
